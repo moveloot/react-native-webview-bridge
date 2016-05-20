9f296cd3c79e658d9ccd10cad602d0719964740d
Ryan Smith <ryanhobsonsmith@gmail.com>
2016-05-20 10:29:56 -0700

    add prop

--- webview-bridge/index.android.js
+++ webview-bridge/index.android.js
@@ -55,6 +55,7 @@ var WebViewBridge = React.createClass({
      * Will be called once the message is being sent from webview
      */
     onBridgeMessage: PropTypes.func,
+    uploadEnabledAndroid: PropTypes.bool,
   },
 
   getInitialState: function() {
